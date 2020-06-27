b5797f98a8f0ba473e629c2b207f6948b8329363
jozeless <jlrodriguezsanabria@gmail.com>
2020-06-27 17:46:41 +0200

    Update with the actual joke

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const app = express()
+
+const page = ` <!DOCTYPE html>
+<html>
+<body>
+<title>Jose Luis</title>
+<h1>Why do Java Programmers wear glasses?</h1>
+<p>Because they don't C#.</p>
+
+</body>
+</html> `
+
+app.get(
+    '/',
+    (request, response) => {
+        response.send(page)
+    }
+    )
+
+
+    const port = 3000
+
+    app.listen(
+port, 
+() => {
+    console.log(`listening on port :${port}`)
+}
+    )
